99f88795ec850e28b71047e10d920b616441824d
fatemeeed <fatemedayian@yahoo.com>
2024-02-26 10:06:46 +0330

    first commit

---
+++ api/v1/cities/index.php
@@ -0,0 +1,24 @@
+<?php
+
+use App/Iran;
+use App/Utilities/Response;
+
+$request_method=$_SERVER['REQUEST_METHOD'];
+
+$request_body=json_decode(file_get_contents('php://input'), true);
+
+switch ($request_method) {
+    case 'GET':
+        $province_id=$_GET['province_id'] ?? null;
+
+        $data=[
+            'province_id' => $province_id
+        ];
+        $response=getCities($data);
+         return Response::respondAndDie($response,Response::HTTP_OK);
+        break;
+    
+    default:
+        # code...
+        break;
+}
---
+++ app/iran.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+use PDO;
+use PDOException;
+
+// <!-- pdo conection -->
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=iran", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+// Validation
+function isValidCity($data)
+{
+    if (empty($data['province_id']) || !is_numeric($data['province_id']))
+        return false;
+
+    return empty($data['name']) ? false : true;
+}
+function isValidProvince($data)
+{
+
+    return empty($data['name']) ? false : true;
+}
+
+function getCities($data)
+{
+    global $conn;
+    $province_id=$data['province_id'] ?? null;
+    $where='';
+
+    if(!is_null($province_id) || is_int($province_id)){
+        $where="province_id={$province_id} ";
+    }
+
+    $sth = $conn->prepare('SELECT * FROM foo');
+    $sth->execute();
+    $records=$sth->fetchAll(PDO::FETCH_OBJ);
+
+    return $records;
+
+}
+function getProvince()
+{
+
+}
